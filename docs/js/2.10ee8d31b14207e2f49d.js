9ad24e5a2eafe9c4032ae632c8084edca49e6b9b
Johannes Drechsler <JohannesDrechslerUS@gmail.com>
2017-09-28 10:31:09 -0400

    Add files via upload

---
+++ docs/js/2.10ee8d31b14207e2f49d.js
@@ -0,0 +1,265 @@
+webpackJsonp([2],{
+
+/***/ 4:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_vue_class_component__ = __webpack_require__(45);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_vue_class_component___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_vue_class_component__);
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+let Car = class Car extends __WEBPACK_IMPORTED_MODULE_0_vue__["default"] {
+    constructor() {
+        super(...arguments);
+        this.chips = ['A/C'];
+        this.today = new Date;
+        this.pickupTime = new Date;
+        this.selectedLocation = 'rw';
+        this.selectedOption = [
+            {
+                label: 'D-04519 RACKWITZ',
+                value: 'rw',
+                description: `
+				SAT <br>
+				Sächsische Autotransport und Service GmbH<br>
+				Am Schafteich 1<br>
+				08371 Glauchau<br>
+				Deutschland
+			`
+            },
+            {
+                label: 'D-04639 GÖSSNITZ',
+                value: 'gn',
+                description: `
+				SAT<br>
+				Sächsische Autotransport und Service GmbH<br>
+				Am Schafteich 1<br>
+				08371 Glauchau<br>
+				Deutschland
+			`
+            },
+            {
+                label: 'D-08371 GLAUCHAU',
+                value: 'gh',
+                description: `
+				SAT<br>
+				Sächsische Autotransport und Service GmbH<br>
+				Am Schafteich 1<br>
+				08371 Glauchau<br>
+				Deutschland
+			`
+            },
+            {
+                label: 'D-14669 KETZIN',
+                value: 'kz',
+                description: `
+				ACM Auto-Service und Umschlag-Center Mosolf Etzin GmbH<br>
+				Gewerbegebiet Etzin<br>
+				14669 Ketzin<br>
+				Deutschland
+			`
+            },
+            {
+                label: 'D-26723 EMDEN',
+                value: 'ed',
+                description: `
+				ALM<br>
+				Automobil-Logistik-Mosolf GmbH<br>
+				Zum Emskai 1<br>
+				26723 Emden<br>
+				Deutschland
+			`
+            },
+            {
+                label: 'D-38442 WOLFSBURG',
+                value: 'wb',
+                description: `
+				MOSOLF Logistics & Services GmbH<br>
+				Niederlassung Wolfsburg<br>
+				Bremerslust 1<br>
+				38442 Wolfsburg<br>
+				Deutschland	
+			`
+            }
+        ];
+    }
+    get getLocationInfo() {
+        var locationInfo = 'not set';
+        for (var index = 0; index < this.selectedOption.length; index++) {
+            var element = this.selectedOption[index];
+            if (element.value === this.selectedLocation) {
+                if (element.description) {
+                    locationInfo = element.description;
+                }
+            }
+        }
+        return locationInfo;
+    }
+    mounted() {
+        console.log('Car mounted');
+    }
+};
+Car = __decorate([
+    __WEBPACK_IMPORTED_MODULE_1_vue_class_component___default.a
+], Car);
+/* harmony default export */ __webpack_exports__["default"] = (Car);
+
+
+/***/ }),
+
+/***/ 45:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/**
+  * vue-class-component v5.0.2
+  * (c) 2015-2017 Evan You
+  * @license MIT
+  */
+
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var Vue = _interopDefault(__webpack_require__(1));
+
+function createDecorator(factory) {
+    return function (target, key, index) {
+        var Ctor = target.constructor;
+        if (!Ctor.__decorators__) {
+            Ctor.__decorators__ = [];
+        }
+        if (typeof index !== 'number') {
+            index = undefined;
+        }
+        Ctor.__decorators__.push(function (options) { return factory(options, key, index); });
+    };
+}
+function warn(message) {
+    if (typeof console !== 'undefined') {
+        console.warn('[vue-class-component] ' + message);
+    }
+}
+
+function collectDataFromConstructor(vm, Component) {
+    Component.prototype._init = function () {
+        var _this = this;
+        var keys = Object.getOwnPropertyNames(vm);
+        if (vm.$options.props) {
+            for (var key in vm.$options.props) {
+                if (!vm.hasOwnProperty(key)) {
+                    keys.push(key);
+                }
+            }
+        }
+        keys.forEach(function (key) {
+            if (key.charAt(0) !== '_') {
+                Object.defineProperty(_this, key, {
+                    get: function () { return vm[key]; },
+                    set: function (value) { return vm[key] = value; }
+                });
+            }
+        });
+    };
+    var data = new Component();
+    var plainData = {};
+    Object.keys(data).forEach(function (key) {
+        if (data[key] !== undefined) {
+            plainData[key] = data[key];
+        }
+    });
+    if (false) {
+        if (!(Component.prototype instanceof Vue) && Object.keys(plainData).length > 0) {
+            warn('Component class must inherit Vue or its descendant class ' +
+                'when class property is used.');
+        }
+    }
+    return plainData;
+}
+
+var $internalHooks = [
+    'data',
+    'beforeCreate',
+    'created',
+    'beforeMount',
+    'mounted',
+    'beforeDestroy',
+    'destroyed',
+    'beforeUpdate',
+    'updated',
+    'activated',
+    'deactivated',
+    'render'
+];
+function componentFactory(Component, options) {
+    if (options === void 0) { options = {}; }
+    options.name = options.name || Component._componentTag || Component.name;
+    var proto = Component.prototype;
+    Object.getOwnPropertyNames(proto).forEach(function (key) {
+        if (key === 'constructor') {
+            return;
+        }
+        if ($internalHooks.indexOf(key) > -1) {
+            options[key] = proto[key];
+            return;
+        }
+        var descriptor = Object.getOwnPropertyDescriptor(proto, key);
+        if (typeof descriptor.value === 'function') {
+            (options.methods || (options.methods = {}))[key] = descriptor.value;
+        }
+        else if (descriptor.get || descriptor.set) {
+            (options.computed || (options.computed = {}))[key] = {
+                get: descriptor.get,
+                set: descriptor.set
+            };
+        }
+    });
+    (options.mixins || (options.mixins = [])).push({
+        data: function () {
+            return collectDataFromConstructor(this, Component);
+        }
+    });
+    var decorators = Component.__decorators__;
+    if (decorators) {
+        decorators.forEach(function (fn) { return fn(options); });
+    }
+    var superProto = Object.getPrototypeOf(Component.prototype);
+    var Super = superProto instanceof Vue
+        ? superProto.constructor
+        : Vue;
+    return Super.extend(options);
+}
+
+function Component(options) {
+    if (typeof options === 'function') {
+        return componentFactory(options);
+    }
+    return function (Component) {
+        return componentFactory(Component, options);
+    };
+}
+(function (Component) {
+    function registerHooks(keys) {
+        $internalHooks.push.apply($internalHooks, keys);
+    }
+    Component.registerHooks = registerHooks;
+})(Component || (Component = {}));
+var Component$1 = Component;
+
+exports['default'] = Component$1;
+exports.createDecorator = createDecorator;
+
+
+/***/ })
+
+});
---
+++ docs/js/app.js
@@ -0,0 +1,206 @@
+webpackJsonp([8],[
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__node_modules_vue_loader_lib_template_compiler_index_id_data_v_32ad72ec_hasScoped_false_node_modules_vue_loader_lib_selector_type_template_index_0_App_vue__ = __webpack_require__(16);
+var normalizeComponent = __webpack_require__(11)
+/* script */
+var __vue_script__ = null
+/* template */
+
+/* styles */
+var __vue_styles__ = null
+/* scopeId */
+var __vue_scopeId__ = null
+/* moduleIdentifier (server only) */
+var __vue_module_identifier__ = null
+var Component = normalizeComponent(
+  __vue_script__,
+  __WEBPACK_IMPORTED_MODULE_0__node_modules_vue_loader_lib_template_compiler_index_id_data_v_32ad72ec_hasScoped_false_node_modules_vue_loader_lib_selector_type_template_index_0_App_vue__["a" /* default */],
+  __vue_styles__,
+  __vue_scopeId__,
+  __vue_module_identifier__
+)
+
+/* harmony default export */ __webpack_exports__["default"] = (Component.exports);
+
+
+/***/ }),
+/* 1 */,
+/* 2 */,
+/* 3 */,
+/* 4 */,
+/* 5 */,
+/* 6 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 7 */,
+/* 8 */,
+/* 9 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_vue_router__ = __webpack_require__(17);
+
+
+
+__WEBPACK_IMPORTED_MODULE_0_vue__["default"].use(__WEBPACK_IMPORTED_MODULE_1_vue_router__["a" /* default */]);
+function load(compPath) {
+    return () => __webpack_require__(19)(`./${compPath}`).then(m => m.default);
+}
+var routes = [
+    { path: '/billsoverview', component: load('Billsoverview/Billsoverview.vue') },
+    { path: '/odata', component: load('Odata/Odata.vue') },
+    { path: '/car', component: load('Car/Car.vue') },
+    { path: '/', component: load('Billsoverview/Billsoverview.vue') },
+    { path: '*', component: load('Error404.vue') }
+];
+const router = new __WEBPACK_IMPORTED_MODULE_1_vue_router__["a" /* default */]({
+    routes,
+    mode: 'history'
+});
+/* harmony export (immutable) */ __webpack_exports__["a"] = router;
+
+
+
+/***/ }),
+/* 10 */,
+/* 11 */,
+/* 12 */,
+/* 13 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 14 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 15 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__components_App_vue__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_quasar__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__router__ = __webpack_require__(9);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_quasar_extras_material_icons__ = __webpack_require__(7);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_quasar_extras_material_icons___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_quasar_extras_material_icons__);
+
+
+
+
+
+
+if (true) {
+    __webpack_require__(8);
+}
+__webpack_require__(6);
+__WEBPACK_IMPORTED_MODULE_0_vue__["default"].config.productionTip = false;
+__WEBPACK_IMPORTED_MODULE_0_vue__["default"].use(__WEBPACK_IMPORTED_MODULE_2_quasar__["default"], {
+    components: __WEBPACK_IMPORTED_MODULE_2_quasar__,
+    directives: __WEBPACK_IMPORTED_MODULE_2_quasar__
+});
+__WEBPACK_IMPORTED_MODULE_2_quasar__["default"].start(() => {
+    new __WEBPACK_IMPORTED_MODULE_0_vue__["default"]({
+        el: '#app',
+        router: __WEBPACK_IMPORTED_MODULE_3__router__["a" /* router */],
+        render: h => h(__WEBPACK_IMPORTED_MODULE_1__components_App_vue__["default"])
+    });
+});
+
+
+/***/ }),
+/* 16 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{attrs:{"id":"app"}},[_c('q-layout',{ref:"layout",attrs:{"view":"hHh LpR fFf","left-breakpoint":0}},[_c('q-toolbar',{slot:"header"},[_c('q-btn',{attrs:{"flat":""},on:{"click":function($event){_vm.$refs.layout.toggleLeft()}}},[_c('q-icon',{attrs:{"name":"menu"}})],1),_vm._v(" "),_c('q-toolbar-title',[_vm._v("\n\t\t\t\tBill Management\n\t\t\t")])],1),_vm._v(" "),_c('div',{slot:"left"},[_c('q-list',{attrs:{"no-border":"","link":""}},[_c('q-side-link',{attrs:{"item":"","to":"/billsoverview"}},[_c('q-item-side',{attrs:{"icon":"account balance"}}),_vm._v(" "),_c('q-item-main',{attrs:{"label":"Bills Overview"}})],1),_vm._v(" "),_c('q-side-link',{attrs:{"item":"","to":"/odata"}},[_c('q-item-side',{attrs:{"icon":"cached"}}),_vm._v(" "),_c('q-item-main',{attrs:{"label":"Odata Test"}})],1),_vm._v(" "),_c('q-side-link',{attrs:{"item":"","to":"/car"}},[_c('q-item-side',{attrs:{"icon":"motorcycle"}}),_vm._v(" "),_c('q-item-main',{attrs:{"label":"Car Management"}})],1)],1)],1),_vm._v(" "),_c('router-view')],1)],1)}
+var staticRenderFns = []
+var esExports = { render: render, staticRenderFns: staticRenderFns }
+/* harmony default export */ __webpack_exports__["a"] = (esExports);
+
+/***/ }),
+/* 17 */,
+/* 18 */,
+/* 19 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var map = {
+	"./App": [
+		0
+	],
+	"./App.vue": [
+		0
+	],
+	"./Billsoverview/Billsoverview": [
+		3,
+		0
+	],
+	"./Billsoverview/Billsoverview.ts": [
+		3,
+		0
+	],
+	"./Billsoverview/Billsoverview.vue": [
+		22,
+		4
+	],
+	"./Car/Car": [
+		4,
+		2
+	],
+	"./Car/Car.ts": [
+		4,
+		2
+	],
+	"./Car/Car.vue": [
+		23,
+		6
+	],
+	"./Error404": [
+		12,
+		3
+	],
+	"./Error404.vue": [
+		12,
+		3
+	],
+	"./Odata/Odata": [
+		5,
+		1
+	],
+	"./Odata/Odata.ts": [
+		5,
+		1
+	],
+	"./Odata/Odata.vue": [
+		24,
+		5
+	]
+};
+function webpackAsyncContext(req) {
+	var ids = map[req];
+	if(!ids)
+		return Promise.reject(new Error("Cannot find module '" + req + "'."));
+	return Promise.all(ids.slice(1).map(__webpack_require__.e)).then(function() {
+		return __webpack_require__(ids[0]);
+	});
+};
+webpackAsyncContext.keys = function webpackAsyncContextKeys() {
+	return Object.keys(map);
+};
+module.exports = webpackAsyncContext;
+webpackAsyncContext.id = 19;
+
+/***/ })
+],[15]);
